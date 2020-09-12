fd47ebefab047e2f5a04e99c7da53e48a60195fb
Nishanthravula <nishanth.ravula@outlook.com>
2020-09-13 00:34:20 +0530

    initial commit

---
+++ ipl_matches.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import csv
+
+url = "https://www.iplt20.com/matches/results/men/2011"
+content = requests.get(url)
+f = open('data-2011.csv','w', newline='')
+writer = csv.writer(f)
+soup = BeautifulSoup(content.text,'html.parser')
+writer.writerow(['Home_team','Away_team','Winner'])
+# print(soup.prettify())
+
+tags = soup.findAll('p', {"class" : "result__team-name"})
+
+a = []
+for tag in tags:
+    team=tag.get_text()
+    a.append(team)
+    # print(team)
+# print(a)
+wons = soup.findAll('p', {"class" : "result__outcome u-hide-phablet"})
+
+c = []
+for won in wons:
+    winner = won.get_text()
+    c.append(winner)
+    # print(winner)
+
+home_team = [a[i] for i in range(len(a)) if i % 2 == 0] 
+away_team = [a[i] for i in range(len(a)) if i % 2 != 0]
+
+tableData = [home_team,away_team, c]
+for v in zip(*tableData):
+    print(*v)
+    writer.writerow(v)
